3c69ca0a49f9885c89227d511c54a123cb472700
sunaliseth <sunaliseth01@gmail.com>
2017-10-22 23:52:22 +0100

    Indec, show, and new views and routes added

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,11 @@
 class PicsController < ApplicationController
-  def index
+  before_action :find_pic, only:[:show, :edit, :update, :destroy]
 
+  def index
+    @pics = Pic.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -9,6 +14,12 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "Yes Pic was posted!"
+    else
+      render 'new'
+    end
   end
 
   private
@@ -16,5 +27,7 @@ class PicsController < ApplicationController
   def pic_params
     params.require(:pic).permit(:title, :description)
   end
-
+  def find_pic
+    @pic = Pic.find(params[:id])
+  end
 end
